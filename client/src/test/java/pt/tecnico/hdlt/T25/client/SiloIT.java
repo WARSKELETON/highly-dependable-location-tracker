25fe4d1ea677e5f3240851f1b72069650d59f2ec
André <andre.9a@gmail.com>
2021-03-30 15:25:20 +0100

    Developed test base

---
+++ client/src/test/java/pt/tecnico/hdlt/T25/client/SiloIT.java
@@ -0,0 +1,43 @@
+package pt.tecnico.hdlt.T25.client;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SiloIT extends TestBase {
+
+    // static members
+    // TODO
+
+
+    // one-time initialization and clean-up
+    @BeforeAll
+    public static void oneTimeSetUp(){
+
+    }
+
+    @AfterAll
+    public static void oneTimeTearDown() {
+
+    }
+
+    // initialization and clean-up for each test
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
+    // tests
+
+    @Test
+    public void test() {
+        assertEquals("", "");
+    }
+
+}
---
+++ client/src/test/java/pt/tecnico/hdlt/T25/client/TestBase.java
@@ -0,0 +1,56 @@
+package pt.tecnico.hdlt.T25.client;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import pt.tecnico.hdlt.T25.client.Domain.Client;
+import pt.tecnico.hdlt.T25.client.Domain.SystemInfo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestBase {
+    private static final String TEST_PROP_FILE = "/test.properties";
+    private static final String TEST_GRID_FILE = "/grid.json";
+
+    private static Properties testProps;
+
+    static Client client;
+
+    @BeforeAll
+    public static void oneTimeSetup() throws IOException {
+        testProps = new Properties();
+
+        try {
+            testProps.load(TestBase.class.getResourceAsStream(TEST_PROP_FILE));
+            System.out.println("Test properties:");
+            System.out.println(testProps);
+        }catch (IOException e) {
+            final String msg = String.format("Could not load properties file {}", TEST_PROP_FILE);
+            System.out.println(msg);
+            throw e;
+        }
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            SystemInfo systemInfo = objectMapper.readValue(new File("resources" + TEST_GRID_FILE), SystemInfo.class);
+
+            final String serverHost = testProps.getProperty("server.host");
+            final String serverPort = testProps.getProperty("server.port");
+            final String clientId = testProps.getProperty("client.id");
+
+            client = new Client(serverHost, Integer.parseInt(serverPort), Integer.parseInt(clientId), systemInfo);
+        }
+        catch (IOException e) {
+            System.err.println("Failed tests");
+        }
+    }
+
+    @AfterAll
+    public static void cleanup() {
+
+    }
+
+}
